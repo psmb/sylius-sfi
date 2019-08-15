ebed9c2c0bf641488a3802d9ec0ed6db8f004b79
Dmitri Pisarev <dimaip@gmail.com>
2019-08-15 18:56:47 +0000

    BETA

---
+++ src/Controller/Shop/AboutUsController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Shop;
+
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class AboutUsController
+{
+    /** @var EngineInterface */
+    private $templatingEngine;
+
+    public function __construct(EngineInterface $templatingEngine)
+    {
+        $this->templatingEngine = $templatingEngine;
+    }
+
+    public function indexAction(Request $request): Response
+    {
+        return $this->templatingEngine->renderResponse('@SyliusShop/aboutus.html.twig');
+    }
+}
