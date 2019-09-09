670cef7a1e1be81007de198f56201ff2e28a5961
Dmitri Pisarev <dimaip@gmail.com>
2019-09-09 15:52:15 +0300

    Require online payments for Pickpoint shipment

---
+++ src/PaymentMethodsResolver.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Sylius\Component\Payment\Resolver\PaymentMethodsResolverInterface;
+use Sylius\Component\Payment\Model\PaymentInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+
+final class PaymentMethodsResolver implements PaymentMethodsResolverInterface
+{
+    /** @var RepositoryInterface */
+    private $paymentMethodRepository;
+
+    public function __construct(RepositoryInterface $paymentMethodRepository)
+    {
+        $this->paymentMethodRepository = $paymentMethodRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSupportedMethods(PaymentInterface $payment): array
+    {
+        $allPaymentMethods = $this->paymentMethodRepository->findBy(['enabled' => true]);
+
+        $firstShipmentMethod = $payment->getOrder()->getShipments()->get(0);
+        if (!$firstShipmentMethod) {
+            throw new \Exception("No shipment method selected");
+        }
+        $shipmentMethodCode = $firstShipmentMethod->getMethod()->getCode();
+
+        return array_filter($allPaymentMethods, function ($paymentMethod) use ($shipmentMethodCode) {
+            $paymentMethodCode = $paymentMethod->getCode();
+            // We only support online payments for PICKPOINT shipment type
+            if ($paymentMethodCode !== "cloudpayments" && $shipmentMethodCode === "PICKPOINT") {
+                return false;
+            }
+            return true;
+        });
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports(PaymentInterface $payment): bool
+    {
+        return true;
+    }
+}
