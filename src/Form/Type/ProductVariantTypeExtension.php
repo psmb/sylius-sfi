b8277125bf48c8d738b1dc55ede386c97ea4d364
Dmitri Pisarev <dimaip@gmail.com>
2019-11-26 18:11:57 +0300

    Change to NumberType

--- src/Form/Type/ProductVariantTypeExtension.php
+++ src/Form/Type/ProductVariantTypeExtension.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Form\Type;
 
 use Sylius\Bundle\CoreBundle\Form\Type\Product\ProductVariantType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\AbstractTypeExtension;
 
@@ -14,7 +14,7 @@ final class ProductVariantTypeExtension extends AbstractTypeExtension
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('originalPrice', TextType::class, [
+            ->add('originalPrice', NumberType::class, [
                 'label' => 'Цена без скидки',
                 'required' => false,
             ]);
