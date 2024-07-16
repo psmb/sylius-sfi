229fdca611fe1cac45610bf4a391515b119779fd
Dmitri Pisarev <dimaip@gmail.com>
2024-07-16 09:06:38 +0300

    Cache CDEK

--- src/Controller/Shop/CdekController.php
+++ src/Controller/Shop/CdekController.php
@@ -9,10 +9,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use App\ISDEKservice;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Contracts\Cache\ItemInterface;
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-
 
 define('CDEK_ACCOUNT', \getenv('CDEK_ACCOUNT'));
 define('CDEK_KEY', \getenv('CDEK_KEY'));
@@ -29,21 +30,41 @@ final class CdekController extends Controller
      */
     protected $container;
 
+    /**
+     * @var FilesystemAdapter
+     */
+    private $cache;
+
     public function __construct(ContainerInterface $container, \Doctrine\Common\Persistence\ObjectManager $objectManager)
     {
         $this->container = $container;
         $this->objectManager = $objectManager;
+        $this->cache = new FilesystemAdapter();
     }
 
     public function serviceAction(Request $request): Response
     {
-
         $service = new ISDEKservice(
             CDEK_ACCOUNT,
             CDEK_KEY
         );
+
+        if (isset($_GET['action']) && $_GET['action'] === 'offices') {
+            $cacheKey = 'get_offices_' . md5(json_encode($_GET));
+            $responseContent = $this->cache->get($cacheKey, function (ItemInterface $item) use ($service) {
+                $item->expiresAfter(604800); // Cache for 1 week (7 days * 24 hours * 60 minutes * 60 seconds)
+                ob_start();
+                $service->process($_GET, file_get_contents('php://input'));
+                return ob_get_clean();
+            });
+
+            return new Response($responseContent, 200, ['Content-Type' => 'application/json']);
+        }
+
         $service->process($_GET, file_get_contents('php://input'));
+        return new Response('', 200, ['Content-Type' => 'application/json']);
     }
+
     public function templateAction(Request $request): Response
     {
         $files = scandir($D = __DIR__ . '/tpl');
