61c2fe2608c631a9ff717133fad10bf3f3d0ff2c
Dmitri Pisarev <dimaip@gmail.com>
2025-06-03 09:04:10 +0300

    Fix logging

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,29 +4,18 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
-use Psr\Log\LoggerInterface;
 use Sylius\Bundle\CoreBundle\Doctrine\ORM\UserRepository as BaseUserRepository;
 use Sylius\Component\User\Model\UserInterface;
 
 class UserRepository extends BaseUserRepository
 {
-    private LoggerInterface $logger;
-
-    public function __construct($entityManager, $class, LoggerInterface $logger = null)
-    {
-        parent::__construct($entityManager, $class);
-        $this->logger = $logger;
-    }
-
     /**
      * {@inheritdoc}
      */
     public function findOneByEmail(string $email): ?UserInterface
     {
         // Log to verify custom repository is being used
-        if ($this->logger) {
-            $this->logger->info('Custom UserRepository::findOneByEmail called', ['email' => $email]);
-        }
+        error_log('CUSTOM UserRepository::findOneByEmail called with email: ' . $email);
 
         // Custom implementation - using customer.emailCanonical instead of user.email
         return $this->createQueryBuilder('o')
